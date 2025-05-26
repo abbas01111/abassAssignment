87f3ed7547401bfc89aa7a200308ad1e649cdc07
Alkassim <usmanalkassim01@gmail.com>
2025-05-25 17:04:04 -0700

    commit task2

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//fun main() {
+//   println("hello Abass!")
+//}
+
+//Create a Kotlin program that asks the user to input their name and age, then prints a greeting like
 fun main() {
-   println("hello Abass!")
+    print("Please enter your name: ")
+    val name = readLine()
+
+    print("Please enter your age: ")
+    val age = readLine()?.toIntOrNull()
+
+    println("Hello, $name! You are $age years old!")
 }
