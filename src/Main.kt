7e52646c95084cad980fa8f5b926532373a1abee
Alkassim <usmanalkassim01@gmail.com>
2025-05-25 17:07:07 -0700

    commit task3

--- src/Main.kt
+++ src/Main.kt
@@ -5,12 +5,29 @@
 //}
 
 //Create a Kotlin program that asks the user to input their name and age, then prints a greeting like
+//fun main() {
+//    print("Please enter your name: ")
+//    val name = readLine()
+//
+//    print("Please enter your age: ")
+//    val age = readLine()?.toIntOrNull()
+//
+//    println("Hello, $name! You are $age years old!")
+//}
+
+//Write a program that checks if a given number is even or odd.
+
 fun main() {
-    print("Please enter your name: ")
-    val name = readLine()
+    print("Enter a number: ")
+    val number = readLine()?.toIntOrNull()
 
-    print("Please enter your age: ")
-    val age = readLine()?.toIntOrNull()
-
-    println("Hello, $name! You are $age years old!")
+    if (number == null) {
+        println("Invalid input. Please enter a valid number.")
+    } else {
+        if (number % 2 == 0) {
+            println("$number is an even number.")
+        } else {
+            println("$number is an odd number.")
+        }
+    }
 }
